700b5610423f4ada68eb15888a3e91a024ced4ba
srikar-pasumarthy <srikarp@email.unc.edu>
2023-04-15 16:06:48 -0400

    Added some files.

--- specific_person_fr.py
+++ specific_person_fr.py
@@ -1,9 +1,16 @@
 import cv2
 import face_recognition
 
-# Load the image of the person you want to recognize
-known_image = face_recognition.load_image_file("known_person.jpg")
-known_encoding = face_recognition.face_encodings(known_image)[0]
+# Load the known face images and their encodings
+known_face_encodings = []
+
+for i in range(1, 11):
+    face_image = face_recognition.load_image_file(f"face{i}.jpg")
+    face_encoding = face_recognition.face_encodings(face_image)[0]
+    known_face_encodings.append(face_encoding)
+
+# Initialize the face detection classifier
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 # Start capturing video from the default camera
 cap = cv2.VideoCapture(0)
@@ -12,25 +19,37 @@ while True:
     # Read a frame from the video feed
     ret, frame = cap.read()
 
-    # Convert the frame to RGB format for face_recognition library
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the frame
-    face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Compare face encodings to the known encoding to see if there is a match
-    for face_encoding in face_encodings:
-        matches = face_recognition.compare_faces([known_encoding], face_encoding)
-        if True in matches:
-            # Draw a rectangle around the recognized face
-            top, right, bottom, left = face_locations[matches.index(True)]
-            # Check if the recognized face is your face
-            if top < 200 and bottom > 300 and left < 200 and right > 400:
-                cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+    # Process each detected face
+    for (x, y, w, h) in faces:
+        # Extract the face encoding from the current face
+        current_face_image = frame[y:y+h, x:x+w]
+        current_face_encoding = face_recognition.face_encodings(current_face_image)
+
+        # Compare the current face encoding with the known face encodings
+        match = False
+        if len(current_face_encoding) > 0:
+            for known_face_encoding in known_face_encodings:
+                if face_recognition.compare_faces([known_face_encoding], current_face_encoding[0])[0]:
+                    match = True
+                    break
+
+        # Draw a rectangle around the face
+        if match:
+            # If the face matches with your face, draw a green rectangle
+            color = (0, 255, 0)
+        else:
+            # If the face doesn't match with your face, draw a red rectangle
+            color = (0, 0, 255)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
 
     # Display the resulting frame
-    cv2.imshow('Face Recognition', frame)
+    cv2.imshow('Face Detection', frame)
 
     # Exit the loop if the user presses the 'q' key
     if cv2.waitKey(1) == ord('q'):

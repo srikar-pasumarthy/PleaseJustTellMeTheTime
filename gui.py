8ae3388cbaa36a836bed8c04eef4a48b909c6a08
wastilla <wastilla@email.unc.edu>
2023-04-20 12:47:39 -0400

    Add Loading spinner

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 
 import tkinter as tk
 import time
+import main
 
 
 class DigitalClock(tk.Label):
@@ -23,17 +24,59 @@ class DigitalClock(tk.Label):
         # Schedule the next update in 1000 milliseconds (1 second)
         self.after(1000, self.update_time)
 
+class LoadingSpinner(tk.Canvas):
+    def __init__(self, master=None, size=50, **kwargs):
+        super().__init__(master, width=size, height=size, **kwargs)
+        self.config(highlightthickness=0)
+        self._arc = self.create_arc(
+            (5, 5, size-5, size-5),
+            start=0, extent=60,
+            width=3, style=tk.ARC,
+            outline="#007bff"
+        )
+        self._job = None
+        self._speed = 20
+    
+    def start(self):
+        if self._job is None:
+            self._job = self.after(self._speed, self._animate)
+    
+    def stop(self):
+        if self._job is not None:
+            self.after_cancel(self._job)
+            self._job = None
+            self.itemconfigure(self._arc, extent=60)
+    
+    def _animate(self):
+        extent = int(float(self.itemcget(self._arc, "extent")) + 10) % 360
+        self.itemconfigure(self._arc, extent=extent)
+        self._job = self.after(self._speed, self._animate)
+
+
+
 class gui:
     def create(self):
         # Create the main window
         root = tk.Tk()
 
+        if(main.training):
+            spinner = LoadingSpinner(root)
+            spinner.pack(padx=20, pady=20)
+        else:
         # Create the digital clock label
-        digital_clock = DigitalClock(root)
+            digital_clock = DigitalClock(root)
 
-        # Add the digital clock label to the window
-        digital_clock.pack()
+            # Add the digital clock label to the window
+            digital_clock.pack()
 
         # Start the main event loop
         root.mainloop()
 
+
+# Example usage of spinner
+
+# root = tk.Tk()
+# spinner = LoadingSpinner(root)
+# spinner.pack(padx=20, pady=20)
+# spinner.start()
+# root.mainloop()
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-# from specific_person_fr import *
+from specific_person_fr import *
 from gui import *
 
+#Boolean indicating if model is done training
+training = False
+
 def main():
+    #Create gui window 
     gui().create()
+    
+    #Train Model function will begin video capture once completed
+    faceRecognition.trainModel()
 
 if __name__ == "__main__":
     main()

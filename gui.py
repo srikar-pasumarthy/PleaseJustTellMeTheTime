9db897b2d73dcae327f4902a54b59b1cf9acc81a
wastilla <wastilla@email.unc.edu>
2023-04-20 11:41:08 -0400

    Modularize code

--- gui.py
+++ gui.py
@@ -2,14 +2,16 @@
 import tkinter as tk
 import time
 
+
 class DigitalClock(tk.Label):
     def __init__(self, master=None, **kwargs):
         super().__init__(master, **kwargs)
+
         # Configure the label
-        self.configure(font=('Courier', 125), background='black', foreground='green')
+        self.configure(font=('Courier', 40), background='black', foreground='green')
+
         # Update the time
         self.update_time()
-
 
     def update_time(self):
         # Get the current time
@@ -21,16 +23,17 @@ class DigitalClock(tk.Label):
         # Schedule the next update in 1000 milliseconds (1 second)
         self.after(1000, self.update_time)
 
-# Create the main window
-root = tk.Tk()
-root.geometry("800x480")
-root.configure(bg='black')
-# Create the digital clock label
-digital_clock = DigitalClock(root)
+class gui:
+    def create(self):
+        # Create the main window
+        root = tk.Tk()
 
-# Add the digital clock label to the window
-digital_clock.pack()
+        # Create the digital clock label
+        digital_clock = DigitalClock(root)
 
-# Start the main event loop
-root.mainloop()
+        # Add the digital clock label to the window
+        digital_clock.pack()
+
+        # Start the main event loop
+        root.mainloop()
 
---
+++ main.py
@@ -0,0 +1,8 @@
+# from specific_person_fr import *
+from gui import *
+
+def main():
+    gui().create()
+
+if __name__ == "__main__":
+    main()
